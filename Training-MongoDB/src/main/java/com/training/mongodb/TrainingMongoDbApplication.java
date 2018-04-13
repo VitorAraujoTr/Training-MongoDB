60fd8451ec8b92b5cfbdea5098f9dcad8b290a93
Vitor Araújo <Vitor Arajo@W8-PCN>
2018-04-13 14:06:59 -0300

    Starting Java Training with MongoDB

---
+++ Training-MongoDB/src/main/java/com/training/mongodb/TrainingMongoDbApplication.java
@@ -0,0 +1,12 @@
+package com.training.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingMongoDbApplication.class, args);
+	}
+}
